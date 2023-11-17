cc1fb23fc208c6100f781c0d62aa4ec00dc3ac42
Oleksiy Hrybach <a.a.gribach@gmail.com>
2023-11-17 16:55:32 +0200

    feat: implement infinite move slider to left/right
    Add moving functionality by shifting slides after each animation ending

--- coffee-house/src/js/slider.js
+++ coffee-house/src/js/slider.js
@@ -1,11 +1,41 @@
 const slides = document.querySelectorAll('.slide');
-const prevBtn = document.querySelector('.slide__btn--prev');
-const nextBtn = document.querySelector('.slide__btn--next');
+const prevBtn = document.querySelector('.slider__btn--prev');
+const nextBtn = document.querySelector('.slider__btn--next');
 const dashes = document.querySelectorAll('.slider__dash');
 const slider = document.querySelector('.slides');
 
-let step = 1;
-let activeSlideContent = slides[step].innerHTML;
+const slidesArr = [...slides];
+
+const shiftSlides = (direction) => {
+    let shiftedSlides = [];
+
+    if (direction === 'left') {
+        shiftedSlides = slidesArr.slice(1).concat(slidesArr.slice(0, 1));
+    }
+
+    if (direction === 'right') {
+        const lastSlideIndex = slidesArr.length - 1;
+
+        shiftedSlides = [slidesArr[lastSlideIndex]].concat(
+            slidesArr.slice(0, lastSlideIndex),
+        );
+    }
+
+    // Delete all slides from HTML
+    slides.forEach((slide) => {
+        slider.removeChild(slide);
+    });
+
+    // Insert slides in new order
+    shiftedSlides.forEach((slide) => {
+        slider.appendChild(slide);
+    });
+
+    // Update slidesArr to with slide in new order for the next click
+    shiftedSlides.forEach((slide, index) => {
+        slidesArr[index] = slide;
+    });
+};
 
 const moveLeft = () => {
     slider.classList.add('transition-left');
@@ -23,18 +53,17 @@ prevBtn.addEventListener('click', moveLeft);
 nextBtn.addEventListener('click', moveRight);
 
 slider.addEventListener('animationend', (event) => {
-    let step = 0;
-    if (event.animationName === 'move-left') {
+    const animationDirection = event.animationName;
+
+    if (animationDirection === 'move-left') {
         slider.classList.remove('transition-left');
-        slides[0].innerHTML = slides[1].innerHTML;
-    } else {
-        slider.classList.remove('transition-right');
-        slides[0].innerHTML = slides[2].innerHTML;
+        shiftSlides('left');
     }
 
-    // for (let i = 0; i < slides.length; i++) {
-
-    // }
+    if (animationDirection === 'move-right') {
+        slider.classList.remove('transition-right');
+        shiftSlides('right');
+    }
 
     prevBtn.addEventListener('click', moveLeft);
     nextBtn.addEventListener('click', moveRight);
@@ -43,36 +72,36 @@ slider.addEventListener('animationend', (event) => {
 // let slideIndex = 1;
 
 // const setActiveSlide = (index) => {
-// 	for (let slide of slides) {
-// 		slide.classList.remove('active');
-// 	}
-// 	slides[index - 1].classList.add('active');
+//     for (let slide of slides) {
+//         slide.classList.remove('active');
+//     }
+//     slides[index - 1].classList.add('active');
 // };
 
 // const setActiveDash = (index) => {
-// 	for (let dash of dashes) {
-// 		dash.classList.remove('active');
-// 	}
-// 	dashes[index - 1].classList.add('active');
+//     for (let dash of dashes) {
+//         dash.classList.remove('active');
+//     }
+//     dashes[index - 1].classList.add('active');
 // };
 
 // const showSlides = (n) => {
-// 	if (n > slides.length) {
-// 		slideIndex = 1;
-// 	}
-// 	if (n < 1) {
-// 		slideIndex = slides.length;
-// 	}
-// 	setActiveSlide(slideIndex);
-// 	setActiveDash(slideIndex);
+//     if (n > slides.length) {
+//         slideIndex = 1;
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length;
+//     }
+//     setActiveSlide(slideIndex);
+//     setActiveDash(slideIndex);
 // };
 
 // const plusSlides = (n) => {
-// 	showSlides((slideIndex += n));
+//     showSlides((slideIndex += n));
 // };
 
 // const currentSlide = (n) => {
-// 	showSlides((slideIndex = n));
+//     showSlides((slideIndex = n));
 // };
 
 // nextBtn.addEventListener('click', () => {
@@ -84,9 +113,9 @@ slider.addEventListener('animationend', (event) => {
 // });
 
 // for (let i = 0; i < dashes.length; i++) {
-// 	dashes[i].addEventListener('click', () => {
-// 		currentSlide(i + 1);
-// 	});
+//     dashes[i].addEventListener('click', () => {
+//         currentSlide(i + 1);
+//     });
 // }
 
 // setInterval(() => {
