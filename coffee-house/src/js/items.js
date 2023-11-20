98fc1c0ff87a7e4e985c8b2b15a4c6e20532ccb7
Oleksiy Hrybach <a.a.gribach@gmail.com>
2023-11-20 15:29:16 +0200

    fix: change items appearance behavior
    change css hiding for resize hiding of items after window width become less than 768px

--- coffee-house/src/js/items.js
+++ coffee-house/src/js/items.js
@@ -1,15 +1,30 @@
-import items from '../api/items.json';
+import itemsData from '../api/items.json';
 
 const itemsBlock = document.querySelector('.items');
 const menuTabs = document.querySelectorAll('.btn-tab');
+const loaderBtn = document.querySelector('.menu-loader');
 
-const renderItems = (category) => {
-    items
-        .filter((item) => item.category === category)
-        .map((item) => {
-            itemsBlock.insertAdjacentHTML(
-                'beforeend',
-                `
+let isRendered = false;
+const initialCategory = 'coffee';
+let currentCategory = initialCategory;
+
+// FIXME: fix rerender after resize
+const renderItems = (items, category) => {
+    let filteredItems = items.filter((item) => item.category === category);
+    console.log(isRendered);
+
+    if (window.innerWidth <= 768) {
+        console.log('windows less than 768px');
+        filteredItems = filteredItems.slice(0, 4);
+        isRendered = true;
+    }
+
+    itemsBlock.innerHTML = '';
+
+    filteredItems.map((item) => {
+        itemsBlock.insertAdjacentHTML(
+            'beforeend',
+            `
 			<article class="item">
 			<div class="item__photo">
 				<img class="item__img"
@@ -26,21 +41,68 @@ const renderItems = (category) => {
 			</div>
 		</article>
 			`,
-            );
-        });
+        );
+    });
+};
+
+const showHiddenItems = (items) => {
+    itemsBlock.classList.add('visible');
+    items.forEach((item) => {
+        item.classList.add('visible');
+    });
+};
+
+const hideItems = () => {
+    itemsBlock.classList.remove('visible');
+};
+
+const loadItems = () => {
+    loaderBtn.textContent = '';
+    loaderBtn.classList.remove('menu-loader');
+    loaderBtn.classList.add('loader');
+    setTimeout(() => {
+        const items = document.querySelectorAll('.item');
+        showHiddenItems(items);
+        loaderBtn.classList.add('hide');
+        loaderBtn.classList.remove('loader');
+        loaderBtn.removeEventListener('click', loadItems);
+    }, 2000);
 };
 
 menuTabs.forEach((menuTab) => {
     menuTab.addEventListener('click', (event) => {
         const category = event.currentTarget.dataset.category;
         const activeTab = document.querySelector('.btn-tab--active');
+        currentCategory = category;
 
         activeTab.classList.remove('btn-tab--active');
         menuTab.classList.add('btn-tab--active');
+        hideItems();
 
         itemsBlock.innerHTML = '';
-        renderItems(category);
+
+        renderItems(itemsData, currentCategory);
+
+        loaderBtn.classList.remove('hide');
+        loaderBtn.classList.add('menu-loader');
+        loaderBtn.addEventListener('click', loadItems);
     });
 });
 
-renderItems('coffee');
+loaderBtn.addEventListener('click', loadItems);
+
+window.addEventListener('load', () => {
+    renderItems(itemsData, initialCategory);
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 768 && !isRendered) {
+        renderItems(itemsData, currentCategory);
+        console.log('resized, width less than 768px');
+        isRendered = true;
+    } else {
+        isRendered = false;
+        renderItems(itemsData, currentCategory);
+        ('resized, width more than 768px');
+    }
+});
