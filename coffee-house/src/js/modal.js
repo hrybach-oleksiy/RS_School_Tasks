0356c1d898405c22a29cc9457abddb90a50201f5
Oleksiy Hrybach <a.a.gribach@gmail.com>
2023-12-05 01:08:17 +0200

    feat: finish modal window logic

--- coffee-house/src/js/modal.js
+++ coffee-house/src/js/modal.js
@@ -13,15 +13,16 @@ const itemModalInfoElements = {
     photoElem: document.querySelector('.modal__photo'),
 };
 
+let finalPrice = 0;
+let itemPrice = 0;
+
 const renderItemInfo = (item) => {
-    console.log(item);
     const title = item.name;
     const descr = item.description;
     const sizes = item.sizes;
     const additives = item.additives;
     const price = item.price;
     const photo = item.src;
-    console.log(sizes);
 
     itemModalInfoElements.titleElem.textContent = title;
     itemModalInfoElements.descrElem.textContent = descr;
@@ -35,11 +36,11 @@ const renderItemInfo = (item) => {
     itemModalInfoElements.sizesElem.innerHTML = '';
 
     for (const size in sizes) {
-        console.log(size);
         const sizeData = sizes[size];
-        console.log(sizeData);
         const sizeBtn = document.createElement('button');
         sizeBtn.classList.add('btn-tab');
+        sizeBtn.dataset.price = sizeData[`add-price`];
+        sizeBtn.dataset.category = 'size';
         sizeBtn.innerHTML = `
             <span class="btn-tab__icon">${size.toUpperCase()}</span>
             <span class="btn-tab__title">${sizeData.size}</span>
@@ -51,12 +52,59 @@ const renderItemInfo = (item) => {
 
     additives.forEach((additive, index) => {
         const additiveBtn = document.createElement('button');
+
         additiveBtn.classList.add('btn-tab');
+        additiveBtn.dataset.price = additive[`add-price`];
+        additiveBtn.dataset.category = 'additive';
         additiveBtn.innerHTML = `
             <span class="btn-tab__icon">${index + 1}</span>
             <span class="btn-tab__title">${additive.name}</span>
         `;
         itemModalInfoElements.additivesElem.appendChild(additiveBtn);
+    });
+
+    itemPrice = Number(price);
+    finalPrice = Number(price);
+};
+
+const chooseItemSize = () => {
+    const sizesElems = document.querySelectorAll('[data-category="size"]');
+    const priceElem = document.querySelector('.modal__price');
+
+    sizesElems.forEach((elem) => {
+        elem.addEventListener('click', () => {
+            const activeTab = document.querySelector('.btn-tab--active');
+            const addPrice = Number(elem.dataset.price);
+
+            activeTab.classList.remove('btn-tab--active');
+            elem.classList.add('btn-tab--active');
+            priceElem.textContent = `$${(itemPrice + addPrice).toFixed(2)}`;
+            finalPrice = itemPrice + addPrice;
+        });
+    });
+};
+
+const chooseItemAdditive = () => {
+    const additiveElems = document.querySelectorAll(
+        '[data-category="additive"]',
+    );
+    const priceElem = document.querySelector('.modal__price');
+
+    additiveElems.forEach((elem) => {
+        elem.addEventListener('click', () => {
+            elem.classList.toggle('btn-tab--active-event');
+            const additivePrice = Number(elem.dataset.price);
+
+            if (elem.classList.contains('btn-tab--active-event')) {
+                finalPrice += additivePrice;
+                itemPrice += additivePrice;
+            } else {
+                finalPrice -= additivePrice;
+                itemPrice -= additivePrice;
+            }
+
+            priceElem.textContent = `$${finalPrice.toFixed(2)}`;
+        });
     });
 };
 
@@ -66,6 +114,12 @@ const showModal = (id) => {
     renderItemInfo(currentItem);
     modal.classList.remove('hidden');
     // document.body.style.overflow = 'hidden';
+    itemModalInfoElements.sizesElem.children[0].classList.add(
+        'btn-tab--active',
+    );
+
+    chooseItemSize();
+    chooseItemAdditive();
 };
 
 const closeModal = () => {
@@ -89,3 +143,9 @@ const handleModalClick = (event) => {
 modal.addEventListener('click', handleModalClick);
 items.addEventListener('click', handleItemsClick);
 closeBtn.addEventListener('click', closeModal);
+
+// itemModalInfoElements.sizesElem.forEach((elem) => {
+//     elem.addEventListener('click', (event) => {
+//         console.log(event.target);
+//     });
+// });
