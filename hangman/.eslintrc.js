35ccc2c4ea44bfc4c9d55943295471d8775e8069
Oleksiy Hrybach <a.a.gribach@gmail.com>
2023-12-30 00:18:27 +0200

    init: start task

---
+++ hangman/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        'no-var': 'error',
+        'prettier/prettier': [
+            'warn',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+        'no-undef': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+    },
+};
---
+++ hangman/webpack.config.js
@@ -0,0 +1,132 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+let isDev = process.env.NODE_ENV === 'development';
+let isProd = !isDev;
+const fileName = (ext) =>
+    isDev ? `[name]${ext}` : `[name].[contenthash:8]${ext}`;
+
+module.exports = {
+    entry: {
+        index: path.join(__dirname, 'src', 'index.js'),
+        menu: path.join(__dirname, 'src', 'menu.js'),
+    },
+    mode: 'development',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: `${fileName('.js')}`,
+        // clean: true,
+        assetModuleFilename: `assets/${fileName('[ext]')}`,
+    },
+    optimization: {
+        usedExports: false,
+    },
+    devtool: isProd ? false : 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                use: 'html-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        'postcss-preset-env',
+                                        {
+                                            browsers: 'last 2 versions',
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|webp|svg)$/i,
+                type: 'asset/resource',
+            },
+            // {
+            //     test: /\.svg$/,
+            //     type: 'asset/resource',
+            //     generator: {
+            //         filename: path.join('[name].[contenthash:8][ext]'),
+            //     },
+            // },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            // {
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     use: ['babel-loader'],
+            // },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            inject: true,
+            chunks: ['index'],
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: isProd,
+                removeComments: isProd,
+                removeRedundantAttributes: isProd,
+                useShortDoctype: isProd,
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'menu.html'),
+            inject: true,
+            chunks: ['menu'],
+            filename: 'menu.html',
+            minify: {
+                collapseWhitespace: isProd,
+                removeComments: isProd,
+                removeRedundantAttributes: isProd,
+                useShortDoctype: isProd,
+            },
+        }),
+        new FileManagerPlugin({
+            events: {
+                onStart: {
+                    delete: ['dist'],
+                },
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: `${fileName('.css')}`,
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/assets/images'),
+                    to: path.resolve(__dirname, 'dist/assets'),
+                },
+            ],
+        }),
+    ],
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        port: 9000,
+    },
+};
