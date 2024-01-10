4d0efdb94a1ef2f07b3719551bc84c548befb9be
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-01-10 13:17:57 +0200

    feat: add hints

--- hangman/src/js/Hangman/hints.js
+++ hangman/src/js/Hangman/hints.js
@@ -2,13 +2,18 @@ const hints = [
   'Capital of Turkey',
   'Famous for Big Ben',
   'The city of canals',
-  'Birth town of Salvador Dali',
+  'Birthplace of Salvador Dali',
   'Birthplace of democracy',
   'Known for the Great Wall',
   'City of the Opera House',
   'The land of the rising sun',
   'City of seven hills',
   'Famous for the Eiffel Tower',
+  'Capital of Australia',
+  'The Lion City',
+  'The Pearl of the Danube',
+  'The City of a Thousand Minarets',
+  'The City of a Hundred Spires',
 ];
 
 const answers = [
@@ -22,6 +27,11 @@ const answers = [
   'Tokyo',
   'Rome',
   'Paris',
+  'Canberra',
+  'Singapore',
+  'Budapest',
+  'Cairo',
+  'Prague',
 ];
 
 export { hints, answers };
