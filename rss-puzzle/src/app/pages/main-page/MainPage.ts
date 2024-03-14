75b99632facbe05c14258ce421c25cf79f4b2c12
Hrybach Oleksiy <84387553+hrybach-oleksiy@users.noreply.github.com>
2024-03-14 17:24:44 +0200

    feat: finish the task (#55)

--- rss-puzzle/src/app/pages/main-page/MainPage.ts
+++ rss-puzzle/src/app/pages/main-page/MainPage.ts
@@ -4,9 +4,9 @@ import data from '../../../api/wordsCollectionLevel1.json';
 import { GameData } from '../../../types/interfaces';
 import ResultBlock from '../../components/result-block/ResultBlock';
 import SourceDataBlock from '../../components/source-data-block/SourceDataBlock';
-import assertIsDefined from '../../../utilities/assertIsDefined';
+// import assertIsDefined from '../../../utilities/assertIsDefined';
 import { FormAttribute } from '../../../types/enums';
-import { button } from '../../components/HTMLComponents';
+import { button, div } from '../../components/HTMLComponents';
 
 export default class MainPage extends BaseComponent {
     gameData: GameData = data;
@@ -33,6 +33,10 @@ export default class MainPage extends BaseComponent {
 
     continueButton: BaseComponent = button(['continue-btn', 'btn'], 'Continue');
 
+    checkButton: BaseComponent = button(['check-btn', 'btn'], 'Check Answer');
+
+    guessedElements: HTMLElement[] = [];
+
     constructor() {
         super({
             tag: 'section',
@@ -45,13 +49,16 @@ export default class MainPage extends BaseComponent {
     }
 
     private setPage() {
+        const btnWrapper = div([styles['btn-wrapper']]);
         this.destroyChildren();
         this.continueButton.setAttribute(FormAttribute.DISABLED, 'true');
+        this.checkButton.setAttribute(FormAttribute.DISABLED, 'true');
         this.continueButton.addListener('click', this.handleContinueButton);
+        this.checkButton.addListener('click', this.checkGuess);
         this.resultBlock = new ResultBlock(this.stringLength, this.guessedSentences);
         this.sourceBlock = new SourceDataBlock(this.words);
-
-        this.appendChildren([this.resultBlock, this.sourceBlock, this.continueButton]);
+        btnWrapper.appendChildren([this.checkButton, this.continueButton]);
+        this.appendChildren([this.resultBlock, this.sourceBlock, btnWrapper]);
     }
 
     private getWords(levelNumber: number, sentenceNumber: number) {
@@ -82,18 +89,28 @@ export default class MainPage extends BaseComponent {
 
     private handleWordClick = (event: Event) => {
         const currentWord = event.target as HTMLElement;
-        const resultBlock = this.resultBlock?.getNode();
-        const sourceBlock = this.sourceBlock?.getNode();
+        // const resultBlock = this.resultBlock?.getNode();
+        // const sourceBlock = this.sourceBlock?.getNode();
         const resultBlockTemplates = document.querySelectorAll(`#row-${this.sentence + 1} .result-template`);
         const sourceBlockTemplates = document.querySelectorAll('.source-template');
+
         const currentTemplateId = Number(currentWord.getAttribute(FormAttribute.ID)?.slice(-1));
         let count = 0;
 
-        assertIsDefined(resultBlock);
-        assertIsDefined(sourceBlock);
+        // assertIsDefined(resultBlock);
+        // assertIsDefined(sourceBlock);
+
+        currentWord.classList.remove('match');
+        currentWord.classList.remove('mismatch');
 
         if (currentWord.closest('#result') && currentWord.classList.contains('part')) {
+            const resultParts = document.querySelectorAll('.result-template .part');
+
             currentWord.classList.add(styles['remove-animation']);
+            resultParts.forEach((part) => {
+                part.classList.remove('match');
+                part.classList.remove('mismatch');
+            });
 
             sourceBlockTemplates.forEach((template) => {
                 const sourceBlockTemplateId = Number(template.getAttribute(FormAttribute.ID)?.slice(-1));
@@ -109,6 +126,7 @@ export default class MainPage extends BaseComponent {
             }, 0);
         } else if (currentWord.closest('#source') && currentWord.classList.contains('part')) {
             currentWord.classList.add(styles['remove-animation']);
+
             this.addWordsToGuessed(currentWord);
             resultBlockTemplates[count].append(currentWord);
             count += 1;
@@ -118,21 +136,24 @@ export default class MainPage extends BaseComponent {
                 currentWord.classList.remove(styles['remove-animation']);
             }, 0);
         }
-
         this.checkWin();
+        this.enableCheckButton();
     };
 
     private addWordsToGuessed(word: HTMLElement) {
         const wordText = word.textContent as string;
         this.guessedWordOrder.push(wordText);
+        this.guessedElements.push(word);
     }
 
     private removeWordsFromGuessed(word: HTMLElement) {
         const wordText = word.textContent as string;
         const wordIndex = this.guessedWordOrder.indexOf(wordText);
+        const elementIndex = this.guessedElements.indexOf(word);
 
         if (wordIndex >= 0) {
             this.guessedWordOrder.splice(wordIndex, 1);
+            this.guessedElements.splice(elementIndex, 1);
         }
     }
 
@@ -142,9 +163,34 @@ export default class MainPage extends BaseComponent {
         }
 
         this.isOrderCorrect = this.correctWordOrder.every((value, index) => value === this.guessedWordOrder[index]);
-        console.log(this.isOrderCorrect);
         this.addGuessedSentences();
-        this.changeButtonState();
+        MainPage.changeButtonState(this.continueButton, this.isOrderCorrect);
+    }
+
+    private checkGuess = () => {
+        for (let i = 0; i < this.correctWordOrder.length; i += 1) {
+            const word1 = this.correctWordOrder[i];
+            const word2 = this.guessedWordOrder[i];
+            const currentElement = this.guessedElements[i];
+
+            if (word1 === word2) {
+                console.log(`word ${this.correctWordOrder[i]} match ${this.guessedWordOrder[i]}`);
+
+                currentElement.classList.add('match');
+                currentElement.classList.remove('mismatch');
+            } else {
+                console.log(`word ${this.correctWordOrder[i]} DOES NOT match ${this.guessedWordOrder[i]}`);
+                currentElement.classList.remove('match');
+                currentElement.classList.add('mismatch');
+            }
+        }
+
+        console.log(this.guessedSentences);
+    };
+
+    private enableCheckButton() {
+        const compareCondition = this.correctWordOrder.length === this.guessedWordOrder.length;
+        MainPage.changeButtonState(this.checkButton, compareCondition);
     }
 
     private addGuessedSentences() {
@@ -155,11 +201,11 @@ export default class MainPage extends BaseComponent {
         }
     }
 
-    private changeButtonState() {
-        if (this.isOrderCorrect) {
-            this.continueButton.removeAttribute(FormAttribute.DISABLED);
+    static changeButtonState(btn: BaseComponent, condition: boolean) {
+        if (condition) {
+            btn.removeAttribute(FormAttribute.DISABLED);
         } else {
-            this.continueButton.setAttribute(FormAttribute.DISABLED, 'true');
+            btn.setAttribute(FormAttribute.DISABLED, 'true');
         }
     }
 
@@ -173,6 +219,7 @@ export default class MainPage extends BaseComponent {
             this.guessedSentences = [];
         }
 
+        this.guessedElements = [];
         this.guessedWordOrder = [];
         this.shuffleWords();
         this.setPage();
